00dbac579f876f9099be31eeb5d8e97eef5e5477
imamdfs <77534254+imamdfs@users.noreply.github.com>
2022-01-24 17:16:59 +0700

    Update starNestedLoop.js

--- starNestedLoop.js
+++ starNestedLoop.js
@@ -1,16 +1,23 @@
-function starNestedLoop(n, m) {
-
-}
-
-//TEST CASE
-starNestedLoop(1,2)
-// *
-// *
-
-starNestedLoop(2,3)
-// **
-// **
-// **
-
-starNestedLoop(4,1)
-// ****
+function starNestedLoop(a, b) {
+    let result = "";
+    for (let i = 0; i < b; i++) {
+      for (let j = 0; j < a; j++) {
+        result += "*";
+      }
+      if (i!==b-1) result += "\n";
+    }
+    console.log(result);
+  }
+  
+  //TEST CASE
+    console.log(starNestedLoop(1,2))
+  // *
+  // *
+  
+    console.log(starNestedLoop(2,3))
+  // **
+  // **
+  // **
+  
+    console.log(starNestedLoop(4,1))
+  // ****
